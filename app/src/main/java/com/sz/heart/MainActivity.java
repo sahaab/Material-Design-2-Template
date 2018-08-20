01ff9287e73684d7cff561740215bdaf226f9070
sahaab <sahaabzahid@gmail.com>
2018-08-20 21:58:45 +0500

    Added Dark Theme and other bug fixes

--- app/src/main/java/com/sz/heart/MainActivity.java
+++ app/src/main/java/com/sz/heart/MainActivity.java
@@ -1,27 +1,38 @@
 package com.sz.heart;
 
-import android.graphics.Typeface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mThemeId = 0;
+
+    private final String PREF_THEME = "theme";
+
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        mThemeId = sharedPreferences.getInt(PREF_THEME,0);
+        if (mThemeId == 1) {
+            this.setTheme(R.style.AppTheme_Dark);
+            getWindow().getDecorView().setSystemUiVisibility(0);
+        } else {
+            this.setTheme(R.style.AppTheme_Light);
+        }
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        TextView mTitle = toolbar.findViewById(R.id.toolbar_title);
-        Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/product_sans_bold.ttf");
-        mTitle.setTypeface(custom_font);
-
     }
 
     @Override
@@ -39,7 +50,16 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_dark) {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            if (mThemeId == 1) {
+                mThemeId = 0;
+            } else {
+                mThemeId = 1;
+            }
+            editor.putInt(PREF_THEME, mThemeId);
+            editor.apply();
+            this.recreate();
             return true;
         }
 
